4f221f40acaa6db3cec3a39fd67e6459fe0543bc
Cade Barnes <31513801+CadeJnBarnes@users.noreply.github.com>
2017-11-15 08:41:28 -0700

    Starting to add the app Panel.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -6,18 +6,24 @@ import javax.swing.JFrame;
 public class ChatFrame extends JFrame
 {
 	private Controller appController;
+//	private ChatPanel appPanel;
 	
 	public ChatFrame(Controller appController)
 	{
 		super();
 		this.appController = appController;	
+//		appPanel = new ChatPanel(appController);
 	
 		setupFrame();
 	}
-	
+	 
 	
 	private void setupFrame()
 	{
+		this.setSize(500,500);
+		this.setTitle("Chatbot 2017");
+//		this.setContentPane(appPanel);
+		this.setResizable(false)
 		this.setVisible(true);
 	}
 }
