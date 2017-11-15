3aaa218998b080fd77f83fef2073cfcedab1d2da
Cade Barnes <31513801+CadeJnBarnes@users.noreply.github.com>
2017-11-15 09:15:31 -0700

    Uncommented Panel stuff from Frame.

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -24,7 +24,7 @@ public class ChatPanel extends JPanel
 		//Initialize GUI data members
 		chatButton = new JButton("chat");
 		chatArea = new JTextArea(10,15);
-		inputField = new JTextField(25);
+		inputField = new JTextField(20);
 		appLayout = new SpringLayout();
 		
 		
