e9a62be7f82065f4feaad8dca840b3b95ca54737
Cade Barnes <31513801+CadeJnBarnes@users.noreply.github.com>
2017-11-15 09:13:14 -0700

    Initiaized all data members.

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -14,12 +14,20 @@ public class ChatPanel extends JPanel
 	private JButton chatButton;
 	private JTextField inputField;
 	private JTextArea chatArea;
-	private SpringLayout baseLayout;
+	private SpringLayout appLayout;
 	
 	public ChatPanel(Controller appController)
 	{
 		super();
 		this.appController = appController;
+		
+		//Initialize GUI data members
+		chatButton = new JButton("chat");
+		chatArea = new JTextArea(10,15);
+		inputField = new JTextField(25);
+		appLayout = new SpringLayout();
+		
+		
 		
 		setupPanel();
 		setupLayout();
